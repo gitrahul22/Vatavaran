c59d3ebf53837c820110e674e4834b975924933a
gitrahul22 <113250125+gitrahul22@users.noreply.github.com>
2024-07-21 23:59:47 +0530

    Update logics.js
    solved the bug

--- logics.js
+++ logics.js
@@ -4,11 +4,17 @@ const search=document.querySelector(".search input");
 const searchbtn=document.querySelector(".search button");
 const weticn=document.querySelector(".weather-icon");
 async function abhowa(cityname){
-        const response=await fetch(apiUrl + `q={cityname}`+`&appid=&{apikey}`)
+        const response=await fetch(apiUrl + `q=${cityname}`+`&appid=${apiKey}`);
+        if(response.status==404) {
+            document.querySelector(".varan").style.display="none";
+           alert("NOT FOUND");
+            
+            return false;
+        }
         var data =await response.json();
         console.log(data);
         document.querySelector(".city").innerHTML=cityname;
-        document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+        document.querySelector(".temp").innerHTML=Math.round((data.main.temp)*0.1)+" °C";
         document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
         document.querySelector(".wind").innerHTML=data.wind.speed+"Kmph";
         if(data.weather[0].main=="Clouds"){
